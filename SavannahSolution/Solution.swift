3f9a86222d2b7b202144fa7ec76b971c8fdb15b9
Savannah Su <tingchen0626@gmail.com>
2021-06-21 23:35:25 +0800

    Fix comments

--- SavannahSolution/Solution.swift
+++ SavannahSolution/Solution.swift
@@ -20,7 +20,7 @@ struct Test1: TestEasy1Wrapper {
     
     // MARK: Solution 2(success)
     ///example: [1, 2, 3, 4, 5], target is 9
-    ///第一次 for loop 找到 1，需要往下找到 9，如果有找到，就 return 兩者的 index
+    ///第一次 for loop 找到 1，需要往下找到 8，如果有找到，就 return 兩者的 index
     ///沒找到，將 1 和其 index 存起來，接下來走第二次 for loop
     ///找到 2，需要往下找到 7 ，以此類推
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
