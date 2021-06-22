2259c6d3d41ba1c772aa6459a5f38b3353444893
Wei <41205mw@gmail.com>
2021-06-23 00:50:22 +0800

    Update: Test Medium 6 - Wei

--- WeiSolution/Solution.swift
+++ WeiSolution/Solution.swift
@@ -14,6 +14,8 @@ public struct Solution: DailyLeetCodeCompatible {
     
     public var test1: TestEasy1Wrapper? { Test1() }
     
+    public var test6: TestMedium6Wrapper? { Test6() }
+    
 }
 
 struct Test1: TestEasy1Wrapper {
@@ -39,4 +41,38 @@ struct Test1: TestEasy1Wrapper {
     
 }
 
-
+struct Test6: TestMedium6Wrapper {
+    
+    /// Create array of string with count as `numRows`
+    /// Use for-loop for parameter `s`, and store each element into array
+    /// with index from `0` to `numRows-1` and then  from `numRows-1` to `0`
+    /// til store last element.
+    /// So if `index == 0` that mean next index plus 1
+    /// else if `index == numRows-1` that mean next index minus 1
+    /// and use flag `isIncreasable` to keey current
+    func convert(_ s: String, _ numRows: Int) -> String {
+        // Edge conditions
+        guard numRows > 1 else { return s }
+        
+        var result = Array(repeating: "", count: numRows)
+        var index = 0
+        var isIncreasable = true
+        for char in s {
+            
+            result[index] = "\(result[index])\(char)"
+            
+            if isIncreasable {
+                index += 1
+            } else {
+                index -= 1
+            }
+            
+            if index == 0 || index == numRows - 1 {
+                isIncreasable.toggle()
+            }
+        }
+        
+        return result.joined()
+    }
+    
+}
--- WeiSolutionTests/WeiSolutionTests.swift
+++ WeiSolutionTests/WeiSolutionTests.swift
@@ -12,12 +12,23 @@ class WeiSolutionTests: XCTestCase {
 
     func test1() throws {
         guard let test = Solution().test1 else {
-            assertionFailure("not found the property test1")
+            assertionFailure("not found the property in \(#function)")
             return
         }
         self.measure {
             let result = test.twoSum([2,7,11,15], 9)
             XCTAssert(result == [0,1])
+        }
+    }
+    
+    func test6() throws {
+        guard let test = Solution().test6 else {
+            assertionFailure("not found the property in \(#function)")
+            return
+        }
+        self.measure {
+            let result = test.convert("PAYPALISHIRING", 3)
+            XCTAssert(result == "PAHNAPLSIIGYIR")
         }
     }
 
