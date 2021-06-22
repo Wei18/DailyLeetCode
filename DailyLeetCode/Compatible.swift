7e2087941e0658a79902db19e1d4af13a1074e4a
Wei <41205mw@gmail.com>
2021-06-23 00:23:03 +0800

    Add: medium 6, and rename file

---
+++ DailyLeetCode/Compatible.swift
@@ -0,0 +1,16 @@
+//
+//  Compatible.swift
+//  DailyLeetCode
+//
+//  Created by ZhiWei Cao on 2021/6/21.
+//
+
+import Foundation
+
+public protocol DailyLeetCodeCompatible {
+   
+    var test1: TestEasy1Wrapper? { get }
+    
+    var test6: TestMedium6Wrapper? { get }
+    
+}
---
+++ DailyLeetCode/Medium/6. ZigZag Conversion.swift
@@ -0,0 +1,17 @@
+//
+//  6. ZigZag Conversion.swift
+//  DailyLeetCode
+//
+//  Created by ZhiWei Cao on 2021/6/23.
+//
+
+import Foundation
+
+public protocol TestMedium6Wrapper {
+    
+    /// 6. ZigZag Conversion
+    ///
+    /// [Reference](https://leetcode.com/problems/zigzag-conversion/)
+    func convert(_ s: String, _ numRows: Int) -> String
+    
+}
