33e2dff7fa65bbdd488ba9f8451ec8d3734846ef
Savannah Su <tingchen0626@gmail.com>
2021-06-26 17:31:00 +0800

    Add test13

---
+++ DailyLeetCode/Easy/13. Roman to Integer.swift
@@ -0,0 +1,18 @@
+//
+//  13. Roman to Integer.swift
+//  DailyLeetCode
+//
+//  Created by Savannah Su on 2021/6/26.
+//
+
+import Foundation
+
+public protocol TestEasy13Wrapper {
+    
+    /// 13. Roman to Integer
+    ///
+    /// [Reference](https://leetcode.com/problems/roman-to-integer/)
+    func romanToInt(_ s: String) -> Int
+    
+}
+
