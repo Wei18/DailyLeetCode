4223c472dff620cc418be95bed5dbf9ee0cb37e8
Savannah Su <tingchen0626@gmail.com>
2021-06-29 21:11:30 +0800

    Add test20

---
+++ DailyLeetCode/Easy/20. Valid Parentheses.swift
@@ -0,0 +1,17 @@
+//
+//  20. Valid Parentheses.swift
+//  DailyLeetCode
+//
+//  Created by Savannah Su on 2021/6/29.
+//
+
+import Foundation
+
+public protocol TestEasy20Wrapper {
+    
+    /// 20. Valid Parentheses
+    ///
+    /// [Reference](https://leetcode.com/problems/valid-parentheses/)
+    func isValid(_ s: String) -> Bool
+    
+}
